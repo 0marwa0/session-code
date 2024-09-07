c659ea7d7fb0c3ac594b1dc777e6ed394ad0ebf0
Marwa Jawad <marwajawad@Marwas-MacBook-Pro.local>
2024-09-07 15:53:25 +0400

    adding burger button

--- portfilio/main.js
+++ portfilio/main.js
@@ -1,1 +1,15 @@
+const burgerBtn = document.getElementById("burger");
+const closeBtn = document.getElementById("close-svg");
 
+const nav_items = document.querySelector(".nav-items");
+burgerBtn.addEventListener("click", () => {
+  nav_items.classList.toggle("open");
+  burgerBtn.style.display = "none";
+  closeBtn.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+  nav_items.classList.toggle("open");
+  burgerBtn.style.display = "block";
+  closeBtn.style.display = "none";
+});
