ccd040e982b0023e03fc07bf0cbf1e8d725cfcda
Marwa Jawad <marwajawad@Marwas-MacBook-Pro.local>
2024-09-22 10:37:24 +0400

    add functions

--- cal/index.js
+++ cal/index.js
@@ -1,1 +1,14 @@
+let input = document.getElementById("input");
+let mathText = "";
+function getValue(x) {
+  mathText += x;
+  input.value = mathText;
+}
+function showResult() {
+  let result = eval(mathText);
+  input.value = result;
+}
 
+function clearText() {
+  input.value = "";
+}
